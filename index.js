6839c06329b07561797b9d7ee6b24e7b0dfb1946
ldh7228 <ldh7228@gmail.com>
2021-07-28 22:34:53 +0900

    Fix win event

--- index.js
+++ index.js
@@ -9,11 +9,14 @@ const novice = document.getElementById('novice');
 const master = document.getElementById('master');
 
 let num = 0;
+let beforePlayerNumber = 0;
 let select = false;
 let easy = false;
 let hard = false;
 let win;
 let game = true;
+let stop = false;
+let robotBeforeNumber = 0;
 
 playerNumber.innerHTML = `${num}`;
 
@@ -31,17 +34,31 @@ function start() {
         comparison();
         robotNumber.innerHTML = `컴퓨터는 ${randomNumber}를 선택하였습니다. ${num}`
         number.innerHTML = `${num}`;
+        robotBeforeNumber = randomNumber;
     }
     if (hard && game) {
+        let robotWin = false;
         for (let i = 1; i < 4; i++) {
             for (let j = 0; j < 8; j++) {
                 if (num + i == (4 * j) + 2) {
                     num += i;
+                    win = false;
                     comparison();
                     robotNumber.innerHTML = `컴퓨터는 ${i}를 선택하였습니다. ${num}`
                     number.innerHTML = `${num}`;
+                    robotBeforeNumber = i;
+                    robotWin = true;
                 }
             }
+        }
+        if (!robotWin) {
+            randomNumber = getRandomIntInclusive(1, 3);
+            num += randomNumber;;
+            win = false;
+            comparison();
+            robotNumber.innerHTML = `컴퓨터는 ${randomNumber}를 선택하였습니다. ${num}`
+            number.innerHTML = `${num}`;
+            robotBeforeNumber = randomNumber;
         }
     }
 }
@@ -53,7 +70,10 @@ function comparison() {
             winnning.innerHTML = `당신이 이겼습니다.`
         }
         else {
+            finalNumber = num - beforePlayerNumber;
             winnning.innerHTML = `당신이 졌습니다.`
+            playerNumber.innerHTML = `컴퓨터는 ${robotBeforeNumber}를 선택하였습니다. ${finalNumber}`;
+            robotNumber.innerHTML = `당신은 ${beforePlayerNumber}을 선택하였습니다. ${num}`;
         }
         number.innerHTML = `${num}`;
         game = false;
@@ -78,8 +98,9 @@ master.addEventListener('click', function () {
 
 plus1.addEventListener('click', function () {
     if (select && game) {
-        num++;
-        playerNumber.innerHTML = `당신은 1을 선택하였습니다. ${num}`;
+        beforePlayerNumber = 1;
+        num += beforePlayerNumber;
+        playerNumber.innerHTML = `당신은 ${beforePlayerNumber}을 선택하였습니다. ${num}`;
         win = true;
         comparison();
         start();
@@ -91,8 +112,9 @@ plus1.addEventListener('click', function () {
 
 plus2.addEventListener('click', function () {
     if (select && game) {
-        num += 2;
-        playerNumber.innerHTML = `당신은 2를 선택하였습니다. ${num}`;
+        beforePlayerNumber = 2;
+        num += beforePlayerNumber;
+        playerNumber.innerHTML = `당신은 ${beforePlayerNumber}를 선택하였습니다. ${num}`;
         win = true;
         comparison();
         start();
@@ -104,8 +126,9 @@ plus2.addEventListener('click', function () {
 
 plus3.addEventListener('click', function () {
     if (select && game) {
-        num += 3;
-        playerNumber.innerHTML = `당신은 3을 선택하였습니다. ${num}`;
+        beforePlayerNumber = 3;
+        num += beforePlayerNumber;
+        playerNumber.innerHTML = `당신은 ${beforePlayerNumber}을 선택하였습니다. ${num}`;
         win = true;
         comparison();
         start();
